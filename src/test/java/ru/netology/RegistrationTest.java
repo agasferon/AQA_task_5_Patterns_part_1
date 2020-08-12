24e783d3f80b5fa95896c8848b4621aa65917236
Anatoly M <m@m.com>
2020-08-12 16:42:56 +0300

    Comments corrected

--- src/test/java/ru/netology/RegistrationTest.java
+++ src/test/java/ru/netology/RegistrationTest.java
@@ -8,26 +8,16 @@ import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-<<<<<<< HEAD
-
-class RegistrationTest {
-    RandomiseDate randomiseDate = new RandomiseDate();
-=======
 import static ru.netology.DataGenerator.CreateUser.*;
 import static ru.netology.DataGenerator.RandomiseDate.getDateIncrement;
 import static ru.netology.DataGenerator.RandomiseDate.getTomorrowDate;
 
 class RegistrationTest {
->>>>>>> Comments corrected
     public User randomUser;
 
     @BeforeEach
     void setUpAll() {
-<<<<<<< HEAD
-        randomUser = CreateUser.generateUser();
-=======
         randomUser = generateUser();
->>>>>>> Comments corrected
     }
 
     @Test
@@ -35,11 +25,7 @@ class RegistrationTest {
         open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue(randomUser.getCity());
         $("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "a", Keys.DELETE);
-<<<<<<< HEAD
-        String bookingDate = randomiseDate.getDateIncrement();
-=======
         String bookingDate = getDateIncrement();
->>>>>>> Comments corrected
         $("[data-test-id='date'] input").setValue(bookingDate);
         $("[data-test-id='name'] input").setValue(randomUser.getName());
         $("[data-test-id='phone'] input").setValue(randomUser.getPhone());
@@ -55,11 +41,7 @@ class RegistrationTest {
         open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue(randomUser.getCity());
         $("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "a", Keys.DELETE);
-<<<<<<< HEAD
-        String bookingDate = randomiseDate.getDateIncrement();
-=======
         String bookingDate = getDateIncrement();
->>>>>>> Comments corrected
         $("[data-test-id='date'] input").setValue(bookingDate);
         $("[data-test-id='name'] input").setValue(randomUser.getName());
         $("[data-test-id='phone'] input").setValue(randomUser.getPhone());
@@ -69,11 +51,7 @@ class RegistrationTest {
         $("[data-test-id=success-notification]").shouldHave(text("Встреча успешно запланирована на"));
         $("[data-test-id=success-notification]").shouldHave(text(bookingDate));
         $("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "a", Keys.DELETE);
-<<<<<<< HEAD
-        String newBookingDate = randomiseDate.getDateIncrement();
-=======
         String newBookingDate = getDateIncrement();
->>>>>>> Comments corrected
         $("[data-test-id='date'] input").setValue(newBookingDate);
         $$("button").find(exactText("Запланировать")).click();
         $(withText("У вас уже запланирована встреча на другую дату. Перепланировать?")).waitUntil(visible, 15000);
@@ -84,18 +62,11 @@ class RegistrationTest {
 
     @Test
     void shouldNotSendIfCityIsNotAvailable() {
-<<<<<<< HEAD
-        open("http://localhost:9999");
-        $("[data-test-id='city'] input").setValue("Сызрань");
-        $("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "a", Keys.DELETE);
-        $("[data-test-id='date'] input").setValue(randomiseDate.getDateIncrement());
-=======
         randomUser = generateUserWithInvalidCity();
         open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue(randomUser.getCity());
         $("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "a", Keys.DELETE);
         $("[data-test-id='date'] input").setValue(getDateIncrement());
->>>>>>> Comments corrected
         $("[data-test-id='name'] input").setValue(randomUser.getName());
         $("[data-test-id='phone'] input").setValue(randomUser.getPhone());
         $("[data-test-id='agreement']").click();
@@ -105,20 +76,12 @@ class RegistrationTest {
 
     @Test
     void shouldNotSendIfNameIsNotCorrect() {
-<<<<<<< HEAD
-        open("http://localhost:9999");
-        $("[data-test-id='city'] input").setValue(randomUser.getCity());
-        $("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "a", Keys.DELETE);
-        $("[data-test-id='date'] input").setValue(randomiseDate.getDateIncrement());
-        $("[data-test-id='name'] input").setValue("Chuck Norris");
-=======
         randomUser = generateUserWithInvalidName();
         open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue(randomUser.getCity());
         $("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "a", Keys.DELETE);
         $("[data-test-id='date'] input").setValue(getDateIncrement());
         $("[data-test-id='name'] input").setValue(randomUser.getName());
->>>>>>> Comments corrected
         $("[data-test-id='phone'] input").setValue(randomUser.getPhone());
         $("[data-test-id='agreement']").click();
         $$("button").find(exactText("Запланировать")).click();
@@ -127,14 +90,6 @@ class RegistrationTest {
 
     @Test
     void shouldNotSendIfPhoneNumberIsNotCorrect() {
-<<<<<<< HEAD
-        open("http://localhost:9999");
-        $("[data-test-id='city'] input").setValue(randomUser.getCity());
-        $("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "a", Keys.DELETE);
-        $("[data-test-id='date'] input").setValue(randomiseDate.getDateIncrement());
-        $("[data-test-id='name'] input").setValue(randomUser.getName());
-        $("[data-test-id='phone'] input").setValue("+7123");
-=======
         randomUser = generateUserWithInvalidPhone();
         open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue(randomUser.getCity());
@@ -142,7 +97,6 @@ class RegistrationTest {
         $("[data-test-id='date'] input").setValue(getDateIncrement());
         $("[data-test-id='name'] input").setValue(randomUser.getName());
         $("[data-test-id='phone'] input").setValue(randomUser.getPhone());
->>>>>>> Comments corrected
         $("[data-test-id='agreement']").click();
         $$("button").find(exactText("Запланировать")).click();
         $(".input_invalid[data-test-id='phone']").shouldHave(text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
@@ -153,11 +107,7 @@ class RegistrationTest {
         open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue(randomUser.getCity());
         $("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "a", Keys.DELETE);
-<<<<<<< HEAD
-        $("[data-test-id='date'] input").setValue(randomiseDate.getDateIncrement());
-=======
         $("[data-test-id='date'] input").setValue(getDateIncrement());
->>>>>>> Comments corrected
         $("[data-test-id='name'] input").setValue(randomUser.getName());
         $("[data-test-id='phone'] input").setValue(randomUser.getPhone());
         String previousColor = $("[data-test-id='agreement']").getCssValue("color");
@@ -171,19 +121,13 @@ class RegistrationTest {
         open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue(randomUser.getCity());
         $("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "a", Keys.DELETE);
-<<<<<<< HEAD
-        $("[data-test-id='date'] input").setValue(randomiseDate.getTomorrowDate());
-=======
         $("[data-test-id='date'] input").setValue(getTomorrowDate());
->>>>>>> Comments corrected
         $("[data-test-id='name'] input").setValue(randomUser.getName());
         $("[data-test-id='phone'] input").setValue(randomUser.getPhone());
         $("[data-test-id='agreement']").click();
         $$("button").find(exactText("Запланировать")).click();
         $(".input_invalid .input__sub").shouldHave(text("Заказ на выбранную дату невозможен"));
     }
-<<<<<<< HEAD
-=======
 
     @Test
     void shouldSendIfNameWithLetterYo() {
@@ -215,5 +159,4 @@ class RegistrationTest {
         $$("button").find(exactText("Запланировать")).click();
         $(".input_invalid[data-test-id='name']").shouldHave(text("Имя и Фамилия указаны неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
->>>>>>> Comments corrected
 }
